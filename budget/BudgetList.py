6eb4ddf06705b81894f69466b0a4a8f2b30aecb8
Kumar Reddy <z993415@thryv.com>
2021-12-01 15:42:36 -0600

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -50,7 +50,7 @@ def main():
     labels = ['Expenses', 'Overages', 'Budget']
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
     ax.bar(labels, values, color=['green','red','blue'])
-    ax.set_title('your total expenses vs. total budget')
+    ax.set_title('Your total expenses vs. total budget')
     plt.show()
 
 
